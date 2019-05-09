1a6f032c9d59084af9cc6bc3711fbde64b5b23a6
designMoreWeb <divystolia@gmail.com>
2019-05-09 15:02:46 -0400

    Variables_Literals_Constants.java done

--- basics/Variables, Literals, and Constants/Variables_Literals_Constants.java
+++ basics/Variables, Literals, and Constants/Variables_Literals_Constants.java
@@ -2,5 +2,14 @@ public class Variables_Literals_Constants{
     public static void main(String[] args) {
         int firstNumber;
         int secondNumber;
+
+        firstNumber=11;
+        secondNumber=15;
+
+        double average;
+
+        average= (firstNumber+secondNumber) / 2;
+
+        System.out.println(average);
     }
 }
